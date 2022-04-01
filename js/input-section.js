5e04f074bc7af640758a67cdaafee018723ce327
sarthakdhasmana <sarthakmay09@gmail.com>
2022-04-01 13:36:10 +0530

    firestore feature addded, input data saved in an object

--- js/input-section.js
+++ js/input-section.js
@@ -1,57 +1,97 @@
-
 const showHidebtn = document.querySelector('#joinUs_btn');
 const submit = document.querySelector('#submitForm');
 const submit1 = document.querySelector('#submitForm2');
 const formContainer = document.querySelector('.early_access_section');
 
 
-showHidebtn.addEventListener('click', () => {
-    formContainer.style.display = 'block';
-    formContainer.scrollIntoView();
-})
 
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-});
+
+
+
+
+
+
+
 
 var form = document.querySelector('#form');
 var transFirst = document.querySelector('#moveFirst');
 var transSecond = document.querySelector('#moveSecond');
 var transThird = document.querySelector('#moveThird');
 var transForth = document.querySelector('#moveForth');
+var transFirstBack = document.querySelector('#moveFirstback');
+var transSecondBack = document.querySelector('#moveSeondback');
+var transThirdBack = document.querySelector('#moveThirdback');
+var transFourthBack = document.querySelector('#moveFourthback');
 
 transFirst.addEventListener('click', () => {
-    form.style.transform = "translateY(-14em)";
+    form.style.transform = "translateX(-42em)";
 })
 transSecond.addEventListener('click', () => {
-    form.style.transform = "translateY(-28em)";
+    form.style.transform = "translateX(-84em)";
 })
 transThird.addEventListener('click', () => {
-    form.style.transform = "translateY(-42em)";
+    form.style.transform = "translateX(-126em)";
 })
 transForth.addEventListener('click', () => {
-    form.style.transform = "translateY(-56em)";
+    form.style.transform = "translateX(-168em)";
 })
+
+transFirstBack.addEventListener('click', () => {
+    form.style.transform = "translateX(0em)";
+})
+transSecondBack.addEventListener('click', () => {
+    form.style.transform = "translateX(-42em)";
+})
+transThirdBack.addEventListener('click', () => {
+    form.style.transform = "translateX(-84em)";
+})
+transFourthBack.addEventListener('click', () => {
+    form.style.transform = "translateX(-126em)";
+})
+
+
+
+
+
+
+
 
 var form2 = document.querySelector('#form2');
 var transFirst2 = document.querySelector('#moveFirst2');
 var transSecond2 = document.querySelector('#moveSecond2');
 var transThird2 = document.querySelector('#moveThird2');
 var transForth2 = document.querySelector('#moveForth2');
+var transFirstBack2 = document.querySelector('#moveFirstback2');
+var transSecondBack2 = document.querySelector('#moveSeondback2');
+var transThirdBack2 = document.querySelector('#moveThirdback2');
+var transFourthBack2 = document.querySelector('#moveFourthback2');
 
-transFirst2.addEventListener('click', () => {
-    form2.style.transform = "translateY(-14em)";
-})
-transSecond2.addEventListener('click', () => {
-    form2.style.transform = "translateY(-28em)";
-})
-transThird2.addEventListener('click', () => {
-    form2.style.transform = "translateY(-42em)";
-})
-transForth2.addEventListener('click', () => {
-    form2.style.transform = "translateY(-56em)";
-})
 submit1.addEventListener('click', (event) => {
     event.preventDefault();
 });
+transFirst2.addEventListener('click', () => {
+    form.style.transform = "translateX(-42em)";
+})
+transSecond2.addEventListener('click', () => {
+    form.style.transform = "translateX(-84em)";
+})
+transThird2.addEventListener('click', () => {
+    form.style.transform = "translateX(-126em)";
+})
+transForth2.addEventListener('click', () => {
+    form.style.transform = "translateX(-168em)";
+})
+
+// transFirstBack2.addEventListener('click', () => {
+//     form.style.transform = "translateX(0em)";
+// })
+// transSecondBack2.addEventListener('click', () => {
+//     form.style.transform = "translateX(-42em)";
+// })
+// transThirdBack2.addEventListener('click', () => {
+//     form.style.transform = "translateX(-84em)";
+// })
+// transFourthBack2.addEventListener('click', () => {
+//     form.style.transform = "translateX(-126em)";
+// })
 
---
+++ js/input_dataFirebase.js
@@ -0,0 +1,65 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAYemrEFaKFrqU1YbN5Cl8QnXLjKt5pYe8",
+    authDomain: "bb-website-83473.firebaseapp.com",
+    projectId: "bb-website-83473",
+    storageBucket: "bb-website-83473.appspot.com",
+    messagingSenderId: "1049078427872",
+    appId: "1:1049078427872:web:ec7e326d3018cf33d76085",
+    measurementId: "G-18YJG46V4W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// // Initialize Cloud Firestore and get a reference to the service
+// const db = firebase.firestore();
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+
+// send the data in the form of collections
+// var messageRef = db.collection('User_data');
+
+showHidebtn.addEventListener('click', () => {
+    formContainer.style.display = 'block';
+    formContainer.scrollIntoView();
+})
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    var name = GetInputVal('name_input');
+    var email = GetInputVal('email_input');
+    var location = GetInputVal('location_input');
+    var type = GetInputVal('type_input');
+
+    //call the function to save the values on submit
+    saveMessage(name, email, location, type);
+});
+
+function GetInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+//function to save the message to the firebase
+function saveMessage(name, email, location, type) {
+    var newMessageRef = messageRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        location: location,
+        type: type
+    });
+} //call this function when the form is submitted
+
+
