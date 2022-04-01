a637d0508471f573a66193236944abc3bf189399
sarthakdhasmana <sarthakmay09@gmail.com>
2022-04-02 00:59:02 +0530

    added authentication of firebase and input section is now fully functionable

--- js/input-section.js
+++ js/input-section.js
@@ -4,116 +4,50 @@ const submit1 = document.querySelector('#submitForm2');
 const formContainer = document.querySelector('.early_access_section');
 
 
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-=======
-// // //send the data in the form of collection
-// // var messageRef = firebase.database().ref('User_data');
-
-
-showHidebtn.addEventListener('click', () => {
-    formContainer.style.display = 'block';
-    formContainer.scrollIntoView();
-})
-
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    var name = GetInputVal('name_input');
-    var email = GetInputVal('email_input');
-    var location = GetInputVal('location_input');
-    var type = GetInputVal('type_input');
-
-    //call the function to save the values on submit
-    saveMessage(name, email, location, type);
-});
->>>>>>> 16a1715b60bc47c99c1ae1b358a8ca1244487002
-
-function GetInputVal(id) {
-    return document.getElementById(id).value;
-}
-
-//function to save the message to the firebase
-function saveMessage(name, email, location, type) {
-    var newMessageRef = messageRef.push();
-    newMessageRef.set({
-        name: name,
-        email: email,
-        location: location,
-        type: type
-    });
-} //call this function when the form is submitted
-
-
-
-
-
-
-
-
-
-
-
 var form = document.querySelector('#form');
 var transFirst = document.querySelector('#moveFirst');
 var transSecond = document.querySelector('#moveSecond');
 var transThird = document.querySelector('#moveThird');
 var transForth = document.querySelector('#moveForth');
+var transFifth = document.querySelector('#moveFifth');
 var transFirstBack = document.querySelector('#moveFirstback');
 var transSecondBack = document.querySelector('#moveSeondback');
 var transThirdBack = document.querySelector('#moveThirdback');
-var transFourthBack = document.querySelector('#moveFourthback');
+var transFourthBack = document.querySelector('#moveForthback');
+var transFifthBack = document.querySelector('#moveFifthback');
 
 transFirst.addEventListener('click', () => {
-    form.style.transform = "translateX(-42em)";
+    form.style.transform = "translateX(-40em)";
 })
 transSecond.addEventListener('click', () => {
-    form.style.transform = "translateX(-84em)";
+    form.style.transform = "translateX(-80em)";
 })
 transThird.addEventListener('click', () => {
-    form.style.transform = "translateX(-126em)";
+    form.style.transform = "translateX(-120em)";
 })
 transForth.addEventListener('click', () => {
-    form.style.transform = "translateX(-168em)";
-<<<<<<< HEAD
+    form.style.transform = "translateX(-160em)";
+})
+transFifth.addEventListener('click', () => {
+    form.style.transform = "translateX(-200em)";
 })
 
 transFirstBack.addEventListener('click', () => {
     form.style.transform = "translateX(0em)";
 })
 transSecondBack.addEventListener('click', () => {
-    form.style.transform = "translateX(-42em)";
+    form.style.transform = "translateX(-40em)";
 })
 transThirdBack.addEventListener('click', () => {
-    form.style.transform = "translateX(-84em)";
+    form.style.transform = "translateX(-80em)";
 })
 transFourthBack.addEventListener('click', () => {
-    form.style.transform = "translateX(-126em)";
+    form.style.transform = "translateX(-120em)";
+})
+transFifthBack.addEventListener('click', () => {
+    form.style.transform = "translateX(-160em)";
 })
 
-=======
-})
-
-transFirstBack.addEventListener('click', () => {
-    form.style.transform = "translateX(0em)";
-})
-transSecondBack.addEventListener('click', () => {
-    form.style.transform = "translateX(-42em)";
-})
-transThirdBack.addEventListener('click', () => {
-    form.style.transform = "translateX(-84em)";
-})
-transFourthBack.addEventListener('click', () => {
-    form.style.transform = "translateX(-126em)";
-})
-
->>>>>>> 16a1715b60bc47c99c1ae1b358a8ca1244487002
 
 
 
@@ -126,37 +60,44 @@ var transFirst2 = document.querySelector('#moveFirst2');
 var transSecond2 = document.querySelector('#moveSecond2');
 var transThird2 = document.querySelector('#moveThird2');
 var transForth2 = document.querySelector('#moveForth2');
+var transFifth2 = document.querySelector('#moveFifth');
 var transFirstBack2 = document.querySelector('#moveFirstback2');
-var transSecondBack2 = document.querySelector('#moveSeondback2');
+var transSecondBack2 = document.querySelector('#moveSecondback2');
 var transThirdBack2 = document.querySelector('#moveThirdback2');
-var transFourthBack2 = document.querySelector('#moveFourthback2');
+var transFourthBack2 = document.querySelector('#moveForthback2');
+var transFifthBack2 = document.querySelector('#moveFifthback');
 
 submit1.addEventListener('click', (event) => {
     event.preventDefault();
 });
 transFirst2.addEventListener('click', () => {
-    form.style.transform = "translateX(-42em)";
+    form.style.transform = "translateX(-40em)";
 })
 transSecond2.addEventListener('click', () => {
-    form.style.transform = "translateX(-84em)";
+    form.style.transform = "translateX(-80em)";
 })
 transThird2.addEventListener('click', () => {
-    form.style.transform = "translateX(-126em)";
+    form.style.transform = "translateX(-120em)";
 })
 transForth2.addEventListener('click', () => {
-    form.style.transform = "translateX(-168em)";
+    form.style.transform = "translateX(-160em)";
+})
+transFifth2.addEventListener('click', () => {
+    form.style.transform = "translateX(-200em)";
 })
 
-// transFirstBack2.addEventListener('click', () => {
-//     form.style.transform = "translateX(0em)";
-// })
-// transSecondBack2.addEventListener('click', () => {
-//     form.style.transform = "translateX(-42em)";
-// })
-// transThirdBack2.addEventListener('click', () => {
-//     form.style.transform = "translateX(-84em)";
-// })
-// transFourthBack2.addEventListener('click', () => {
-//     form.style.transform = "translateX(-126em)";
-// })
-
+transFirstBack2.addEventListener('click', () => {
+    form.style.transform = "translateX(0em)";
+})
+transSecondBack2.addEventListener('click', () => {
+    form.style.transform = "translateX(-40em)";
+})
+transThirdBack2.addEventListener('click', () => {
+    form.style.transform = "translateX(-80em)";
+})
+transFourthBack2.addEventListener('click', () => {
+    form.style.transform = "translateX(-120em)";
+})
+transFifthBack2.addEventListener('click', () => {
+    form.style.transform = "translateX(-160em)";
+})
--- js/input_dataFirebase.js
+++ js/input_dataFirebase.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,11 +22,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// // Initialize Cloud Firestore and get a reference to the service
-// const db = firebase.firestore();
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+//initialize the google authentication
+const provider1 = new GoogleAuthProvider();
+//initialize the facebook authentication
+const provider2 = new FacebookAuthProvider();
 
 
 // send the data in the form of collections
@@ -37,32 +40,104 @@ showHidebtn.addEventListener('click', () => {
     formContainer.scrollIntoView();
 })
 
+//google_signIn btn
+const googleSignInBtn = document.querySelector('.google_signIn');
+const facebookSignInBtn = document.querySelector('.facebook_signIn');
+const googleSignInBtn2 = document.querySelector('.google_signIn2');
+const facebookSignInBtn2 = document.querySelector('.facebook_signIn2');
+
+googleSignInBtn.addEventListener('click', () => {
+    signInWithGoogle();
+})
+facebookSignInBtn.addEventListener('click', () => {
+    signInWithFacebook();
+})
+googleSignInBtn2.addEventListener('click', () => {
+    signInWithGoogle();
+})
+facebookSignInBtn2.addEventListener('click', () => {
+    signInWithFacebook();
+})
+
 submit.addEventListener('click', (e) => {
     e.preventDefault();
-    var name = GetInputVal('name_input');
-    var email = GetInputVal('email_input');
-    var location = GetInputVal('location_input');
-    var type = GetInputVal('type_input');
-
+    form.style.transform = "translateX(-240em)"
     //call the function to save the values on submit
-    saveMessage(name, email, location, type);
+    addVal();
 });
+
 
 function GetInputVal(id) {
     return document.getElementById(id).value;
 }
+async function addVal() {
+    try {
+        var name = GetInputVal('name_input');
+        var email = GetInputVal('email_input');
+        var location = GetInputVal('location_input');
+        var type = GetInputVal('type_input');
 
-//function to save the message to the firebase
-function saveMessage(name, email, location, type) {
-    var newMessageRef = messageRef.push();
-    newMessageRef.set({
-        name: name,
-        email: email,
-        location: location,
-        type: type
-    });
-} //call this function when the form is submitted
-=======
->>>>>>> 16a1715b60bc47c99c1ae1b358a8ca1244487002
+        const docRef = await addDoc(collection(db, "users"), {
+            name: name,
+            email: email,
+            location: location,
+            type: type
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+};
+
+//function for the google authentication
+function signInWithGoogle() {
+
+    signInWithPopup(auth, provider1)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+
+}
 
 
+//function for the facebook authentication
+function signInWithFacebook() {
+
+    signInWithPopup(auth, provider2)
+        .then((result) => {
+            // The signed-in user info.
+            const user = result.user;
+
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            const credential = FacebookAuthProvider.credentialFromResult(result);
+            const accessToken = credential.accessToken;
+
+            // ...
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = FacebookAuthProvider.credentialFromError(error);
+
+            // ...
+        });
+
+};
