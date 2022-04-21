142416fbfe13d7e625a6ff1df28f6e860920afbe
sarthakdhasmana <sarthakmay09@gmail.com>
2022-04-21 09:57:06 +0530

    privacy pages added, made some small changes

--- js/input_dataFirebase.js
+++ js/input_dataFirebase.js
@@ -368,7 +368,7 @@ function sendEmail(email) {
     const actionCodeSettings = {
         // URL you want to redirect back to. The domain (www.example.com) for this
         // URL must be in the authorized domains list in the Firebase Console.
-        url: 'http://127.0.0.1:5501/',
+        url: 'https://www.blausm.app/',
         // This must be true.
         handleCodeInApp: true,
         // iOS: {
